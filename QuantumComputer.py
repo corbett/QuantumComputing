5ceb5a90465c13c055355747f68defb784bfa97a
Christine Corbett <christine.corbett@gmail.com>
2016-05-10 14:38:02 +1200

    Manually compute and test Bell State exercises. Also added TODOs to handle entangled 2-qubit states in QuantumComputer impelemntation

--- QuantumComputer.py
+++ QuantumComputer.py
@@ -17,6 +17,8 @@ H=1./sqrt(2)*np.matrix('1 1; 1 -1')
 X=np.matrix('0 1; 1 0')
 Y=np.matrix([[0, -i_],[i_, 0]])
 Z=np.matrix([[1,0],[0,-1]])
+W=1/sqrt(2)*(X+Z)
+V=1/sqrt(2)*(-X+Z)
 
 S=np.matrix([[1,0],[0,i_]])
 Sdagger=np.matrix([[1,0],[0,-i_]]) # convenience Sdagger = S.conjugate().transpose()
@@ -41,6 +43,9 @@ minus_state=1/sqrt(2)*np.matrix('1; -1')
 plusi_state=1/sqrt(2)*np.matrix([[1],[i_]])    # also known as clockwise arrow state
 minusi_state=1/sqrt(2)*np.matrix([[1],[-i_]])  # also known as counterclockwise arrow state
 
+# bell state = 1/sqrt(2)(|00>+|11>")
+bell_state = 1/sqrt(2)*np.matrix('1 0; 0 1')
+
 # It seems to me to derive the ordering you do ((+) is outer product):
 # Symbolically: |00> = |0> (+) |0>; gives 2x2 matrix which is then rewritten as 4x1 matrix by enumerate elements of rows from row 0
 # In Python: (zero_state*zero_state.conjugate().transpose()).reshape(4,1)
@@ -49,12 +54,59 @@ two_qubits_01=np.matrix('0; 1; 0; 0')
 two_qubits_10=np.matrix('0; 0; 1; 0')
 two_qubits_11=np.matrix('0; 0; 0; 1')
 
+# To operate a gate which operates on one qubit on elements of entangled 2-qubit state
+qubits_00_state=np.matrix('1 0; 0 0')
+qubits_01_state=np.matrix('0 1; 0 0')
+qubits_10_state=np.matrix('0 0; 1 0')
+qubits_11_state=np.matrix('0 0; 0 1')
+
+def get_probability(coeff):
+	return (coeff*coeff.conjugate()).real
+
+def get_probabilities(state):
+	return [get_probability(x) for x in state.flat]
+
+def get_correlated_expectation(state):
+	probs=get_probabilities(state)
+	return probs[0]+probs[3]-probs[1]-probs[2]
+
+def pretty_print_probabilities(state):
+	if state.shape == (2,2):
+		sa00,sa01,sa10,sa11=state.item(0),state.item(1),state.item(2),state.item(3)
+		p00,p01,p10,p11=[get_probability(x) for x in [sa00,sa01,sa10,sa11]]
+		print """
+		%r|00>+%r|01>+%r|10>+%r|11>:
+		Pr(|00>)=%f; Pr(|01>)=%f; Pr(|10>)=%f; Pr(|11>)=%f
+		<state> = %f
+		""" % (sa00,sa01,sa10,sa11,p00,p01,p10,p11,p00+p11-p01-p10)
+	elif state.shape == (2,1):
+		print """
+		%r|0>+%r|1>:
+		Pr(|0>)=%f
+		Pr(|1>)=%f
+		""" % (state.item(0),
+			state.item(1),
+			get_probability(state.item(0)),
+			get_probability(state.item(1))
+			)
+	elif state.shape == (4,1):
+		pretty_print_probabilities(state.reshape(2,2))
+	else:
+		print "don't recognize this shape cannot pretty print"
 
 def change_to_x_basis(state):
 	return H*state
 
 def change_to_y_basis(state):
 	return H*Sdagger*state
+
+def change_to_w_basis(state):
+	# W=1/sqrt(2)*(X+Z)
+	return H*T*H*S*state
+
+def change_to_v_basis(state):
+	# V=1/sqrt(2)*(-X+Z)
+	return H*Tdagger*H*S*state
 
 def expectation_x(state):
 	state_x=change_to_x_basis(state)
@@ -74,15 +126,20 @@ def expectation_z(state):
 	prob_one_state=(state_z.item(1)*state_z.item(1).conjugate()).real
 	return prob_zero_state-prob_one_state
 
-def get_target_value_cnot(two_qubit_state):
+def get_individual_qubits(two_qubit_state):
 	"""just returns either a zero or one state equal to the target value of the CNOT output. 
 	Throws exception if not a pure two qubit state"""
-	if np.allclose(two_qubit_state,two_qubits_00) or np.allclose(two_qubit_state,two_qubits_10):
-		return zero_state
-	elif np.allclose(two_qubit_state,two_qubits_01) or np.allclose(two_qubit_state,two_qubits_11):
-		return one_state
+	if np.allclose(two_qubit_state,two_qubits_00):
+		return (zero_state,zero_state)
+	elif np.allclose(two_qubit_state,two_qubits_11):
+		return (one_state,one_state)
+	elif np.allclose(two_qubit_state,two_qubits_01):
+		return (zero_state,one_state)
+	elif np.allclose(two_qubit_state,two_qubits_10):
+		return (one_state,zero_state)
 	else:
-		raise ValueError('Input must be a pure two qubit state')
+		# TODO:
+		raise Exception("TODO: Entangled qubits not represented yet in quantum computer implementation. Can currently do manual calculations; see TestBellState for Examples")
 
 def measure(state):
 	"""finally some probabilities, whee. To properly use, set the qubit you measure to the result of this function
@@ -116,6 +173,13 @@ class QuantumComputer(object):
 		self.q2=zero_state
 		self.q3=zero_state
 		self.q4=zero_state
+	def reset():
+		self.q0=zero_state
+		self.q1=zero_state
+		self.q2=zero_state
+		self.q3=zero_state
+		self.q4=zero_state
+
 	def execute(self,program):
 		"""Time for some very lazy meta programming!
 		"""
@@ -137,7 +201,7 @@ class QuantumComputer(object):
 			['measure self.q2',r'measure(self.q2)'],
 			['measure self.q3',r'measure(self.q3)'],
 			['measure self.q4',r'measure(self.q4)'],
-			['id',''],
+			['id ',''],
 			['sdg ','Sdagger*'],
 			['tdg ','Tdagger*'],
 			['h ','H*'],
@@ -162,7 +226,7 @@ class QuantumComputer(object):
 				set_syntax=cnot.group(1)
 				control_qubit=cnot.group(2)
 				target_qubit=cnot.group(3)
-				l='%sget_target_value_cnot(CNOT*(%s*%s.conjugate().transpose()).reshape(4,1))'%(set_syntax,control_qubit,target_qubit)
+				l='%s,%s=get_individual_qubits(CNOT*(%s*%s.conjugate().transpose()).reshape(4,1))'%(control_qubit,target_qubit,control_qubit,target_qubit)
 			for k,v in translation:
 				l=l.replace(k,v)
 			# Now running the code
@@ -242,7 +306,42 @@ class TestTGate(unittest.TestCase):
 		# Checking norms
 		for state in [red_state,green_state,blue_state]:
 			self.assertAlmostEqual(np.linalg.norm(get_bloch(state)),1.0)
+class TestBellState(unittest.TestCase):
+	def test_bell_state(self):
+		# manually computing some of this (verified results to match IBM)
+		zz=bell_state
+		pr_zz=get_probabilities(zz)
+		corex_zz=get_correlated_expectation(zz)
+		self.assertTrue(np.allclose(pr_zz,(0.5,0,0,0.5)))
+		self.assertAlmostEqual(corex_zz,1)
+		
+		zw=change_to_w_basis(bell_state)
+		pr_zw=get_probabilities(zw)
+		corex_zw=get_correlated_expectation(zw)
+		self.assertTrue(np.allclose(pr_zw,(0.426777,0.073223,0.073223,0.426777)))
+		self.assertAlmostEqual(corex_zw,1/sqrt(2))
 
+		zv=change_to_v_basis(bell_state)
+		pr_zv=get_probabilities(zv)
+		corex_zv=get_correlated_expectation(zv)
+		self.assertTrue(np.allclose(pr_zv,(0.426777,0.073223,0.073223,0.426777)))
+		self.assertAlmostEqual(corex_zv,1/sqrt(2))
+
+		xw=change_to_w_basis(change_to_x_basis(bell_state))
+		pr_xw=get_probabilities(xw)
+		corex_xw=get_correlated_expectation(xw) 
+		self.assertTrue(np.allclose(pr_xw,(0.426777,0.073223,0.073223,0.426777)))
+		self.assertAlmostEqual(corex_xw,1/sqrt(2))
+
+		xv=change_to_v_basis(change_to_x_basis(bell_state))
+		pr_xv=get_probabilities(xv)
+		corex_xv=get_correlated_expectation(xv)
+		self.assertTrue(np.allclose(pr_xv,(0.073223,0.426777,0.426777,0.073223)))
+		self.assertAlmostEqual(corex_xv,-1/sqrt(2))
+		# Why is |C| = |<AB>| + |-<AB'>| + |<A'B>| + |<A'B'>|; instead of |<AB> + -<AB'> + <A'B> + <A'B'>|? Otherwise this result doesn't work out to be 2*sqrt(2)
+		abs_chsh_c = abs(corex_zw) + abs(corex_zv) + abs(corex_xw) + abs(-corex_xv) 
+		self.assertAlmostEqual(abs_chsh_c,2*sqrt(2))
+		
 class TestQuantumComputer(unittest.TestCase):
 	def setUp(self):
 		self.qc=QuantumComputer()
@@ -326,9 +425,60 @@ class TestQuantumComputer(unittest.TestCase):
 		self.assertTrue(np.allclose(self.qc.q2,zero_state))
 		self.assertTrue(np.allclose(self.qc.q3,zero_state))
 		self.assertTrue(np.allclose(self.qc.q4,one_state))
+	# Bell state experiments
+	def test_bellstate_measurements(self):
+		#This tests two qubit measurements and entanglement
+
+		program_zz="""h q[1];
+			cx q[1], q[2];
+			measure q[1];
+			measure q[2];""" # "00",0.5; "11",0.5 # <zz> = 2
+		
+		program_zw="""h q[1];
+			cx q[1], q[2];
+			s q[2];
+			h q[2];
+			t q[2];
+			h q[2];
+			measure q[1];
+			measure q[2]""" # "00",0.426777; "01",0.073223; "10",0.073223; "11",0.426777 # <zw> = 1/sqrt(2)
+
+		program_zv="""h q[1];
+			cx q[1], q[2];
+			s q[2];
+			h q[2];
+			tdg q[2];
+			h q[2];
+			measure q[1];
+			measure q[2];""" #"00",0.426777; "01",0.073223; "10",0.073223; "11",0.426777 # <zv> = 1/sqrt(2)
+
+		program_xw="""h q[1];
+			cx q[1], q[2];
+			h q[1];
+			s q[2];
+			h q[2];
+			t q[2];
+			h q[2];
+			measure q[1];
+			measure q[2];""" # "00",0.426777; "01",0.073223; "10",0.073223; "11",0.426777 # <xw> = 
+
+		program_xv="""h q[1];
+			cx q[1], q[2];
+			h q[1];
+			s q[2];
+			h q[2];
+			tdg q[2];
+			h q[2];
+			measure q[1];
+			measure q[2];""" #"00",0.073223; "01",0.426777; "10",0.426777; "11",0.073223; # <xv> =
+		# TODO: modify
+		raise Exception("TODO: Entangled qubits not represented yet in quantum computer implementation. Can currently do manual calculations; see TestBellState for Examples")
+		#self.qc.execute(program)
 
 	def tearDown(self):
 		self.qc=None
+
+
 
 
 if __name__ == '__main__':
