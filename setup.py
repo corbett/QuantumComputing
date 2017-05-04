5bd05c8765a403f9c6785dff5827bc75cf7cfc9b
LSaldyt <lucassaldyt@gmail.com>
2017-05-04 15:29:52 -0600

    Uses setup.py install of install_locally

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='QuantumComputing',
+      version='1.0',
+      py_modules=['QuantumComputer'],
+      description='Simulation for IBM Quantum Experience',
+      author='Dr. Christine Corbett Moran',
+      author_email='corbett@caltech.edu',
+      url='https://github.com/corbett/QuantumComputing',
+  )
